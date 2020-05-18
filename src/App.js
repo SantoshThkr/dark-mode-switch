a3246c99aa9bca741ef0a062aba8396d04c04207
SantoshThkr <santosh.thakur36911@gmail.com>
2020-05-18 10:42:17 +0530

    start movie search app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Movie Search</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
